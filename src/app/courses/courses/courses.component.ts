8d16f254cbb5b0161ba3077822e81cd21ebd6bcb
Alisson Youssf <youssfbr@gmail.com>
2023-11-30 20:54:50 -0300

    Card

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../models/course';
+
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule
+  ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
@@ -14,7 +23,7 @@ export class CoursesComponent implements OnInit {
   courses: Course[] = [
     { _id: "1", name: "Angular", category: "front-end"}
   ];
-  
+
   displayedColumns = ['name', 'category'];
 
   constructor() {}
