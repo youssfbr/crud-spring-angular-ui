2c7cf607feebfe38f428e3fb3c1cbb147063f9fc
Alisson Youssf <youssfbr@gmail.com>
2023-11-30 20:20:39 -0300

    Table mock

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: "1", name: "Angular", category: "front-end"}
+  ];
+  
+  displayedColumns = ['name', 'category'];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
